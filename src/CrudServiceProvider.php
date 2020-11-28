6bf40575b049da5b195735b765c70c2e5124104a
Aleksandr <aleksandr.chyzhevskiy@it-devgroup.com>
2020-11-28 21:00:54 +0200

    up

--- src/CrudServiceProvider.php
+++ src/CrudServiceProvider.php
@@ -20,5 +20,29 @@ class CrudServiceProvider extends ServiceProvider
                 ApiCrudGenerator::class,
             ]);
         }
+        if (method_exists($this, 'loadViewsFrom')) {
+            $this->loadViewsFrom(__DIR__ . '/stubs', 'crud-generator');
+        }
+
+        if (method_exists($this, 'publishes')) {
+            $this->publishes([
+                __DIR__ . '/stubs' => base_path('/resources/stubs'),
+            ], 'views');
+            $this->publishes([
+                __DIR__ . '/config/crud-generator.php' => $this->configPath('crud-generator.php'),
+            ]);
+        }
+    }
+
+    /**
+     * @param string $path
+     * @return string
+     */
+    private function configPath($path = ''): string
+    {
+        return function_exists('config_path')
+            ? config_path($path)
+            : app()->basePath() . DIRECTORY_SEPARATOR . 'config' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
 }
+
