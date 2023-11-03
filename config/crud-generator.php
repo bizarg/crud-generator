4ade93ebc96c53d155b11123b180ba45e3ccd3b0
Chyzhevskiy <aleksandr.chyzhevskiy@it-devgroup.com>
2023-11-03 15:32:35 +0200

    fix

--- config/crud-generator.php
+++ config/crud-generator.php
@@ -5,116 +5,116 @@ return [
         'stub' => null,
     ],
     'templates' => [
-//        [
-//            'path' => 'app/Http/Controllers/Api',
-//            'filename' => '{{modelName}}Controller',
-//            'stub' => 'Controller'
-//        ],
-//        [
-//            'path' => 'app/Domain/{{modelName}}',
-//            'filename' => '{{modelName}}Filter',
-//            'stub' => 'Filter'
-//        ],
-//        [
-//            'path' => 'app/Domain/{{modelName}}',
-//            'filename' => '{{modelName}}',
-//            'stub' => 'Model'
-//        ],
-//        [
-//            'path' => 'app/Domain/{{modelName}}',
-//            'filename' => '{{modelName}}NotFound',
-//            'stub' => 'NotFound'
-//        ],
-//        [
-//            'path' => 'app/Domain/{{modelName}}',
-//            'filename' => '{{modelName}}NotFound',
-//            'stub' => 'NotFound'
-//        ],
-//        [
-//            'path' => 'app/Domain/{{modelName}}',
-//            'filename' => '{{modelName}}RepositoryInterface',
-//            'stub' => 'RepositoryInterface'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Delete{{modelName}}',
-//            'filename' => 'Delete{{modelName}}',
-//            'stub' => 'Delete'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Delete{{modelName}}',
-//            'filename' => 'Delete{{modelName}}Handler',
-//            'stub' => 'DeleteHandler'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Get{{modelName}}List',
-//            'filename' => 'Get{{modelName}}List',
-//            'stub' => 'GetList'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Get{{modelName}}List',
-//            'filename' => 'Get{{modelName}}ListHandler',
-//            'stub' => 'GetListHandler'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Store{{modelName}}',
-//            'filename' => 'Store{{modelName}}',
-//            'stub' => 'Store'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Store{{modelName}}',
-//            'filename' => 'Store{{modelName}}Handler',
-//            'stub' => 'StoreHandler'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Update{{modelName}}',
-//            'filename' => 'Update{{modelName}}',
-//            'stub' => 'Update'
-//        ],
-//        [
-//            'path' => 'app/Application/{{modelName}}/Update{{modelName}}',
-//            'filename' => 'Update{{modelName}}Handler',
-//            'stub' => 'UpdateHandler'
-//        ],
-//        [
-//            'path' => 'app/Http/Requests/{{modelName}}',
-//            'filename' => 'List{{modelName}}Request',
-//            'stub' => 'ListRequest'
-//        ],
-//        [
-//            'path' => 'app/Http/Requests/{{modelName}}',
-//            'filename' => 'Store{{modelName}}Request',
-//            'stub' => 'StoreRequest'
-//        ],
-//        [
-//            'path' => 'app/Http/Requests/{{modelName}}',
-//            'filename' => 'Update{{modelName}}Request',
-//            'stub' => 'UpdateRequest'
-//        ],
-//        [
-//            'path' => 'api-doc/collections/',
-//            'filename' => '{{modelNamePluralLowerCaseHyphen}}',
-//            'stub' => 'ApiDoc'
-//        ],
-//        [
-//            'path' => 'database/migrations/',
-//            'filename' => '{{migrationName}}',
-//            'stub' => 'Migration'
-//        ],
-//        [
-//            'path' => 'tests/Feature/',
-//            'filename' => '{{modelName}}Test',
-//            'stub' => 'Test'
-//        ],
-//        [
-//            'path' => 'app/Http/Resources/{{modelName}}',
-//            'filename' => '{{modelName}}Resource',
-//            'stub' => 'Resource'
-//        ],
-//        [
-//            'path' => 'app/Http/Resources/{{modelName}}',
-//            'filename' => '{{modelName}}ResourceCollection',
-//            'stub' => 'ResourceCollection'
-//        ],
+        [
+            'path' => 'app/Http/Controllers/Api',
+            'filename' => '{{modelName}}Controller',
+            'stub' => 'Controller'
+        ],
+        [
+            'path' => 'app/Domain/{{modelName}}',
+            'filename' => '{{modelName}}Filter',
+            'stub' => 'Filter'
+        ],
+        [
+            'path' => 'app/Domain/{{modelName}}',
+            'filename' => '{{modelName}}',
+            'stub' => 'Model'
+        ],
+        [
+            'path' => 'app/Domain/{{modelName}}',
+            'filename' => '{{modelName}}NotFound',
+            'stub' => 'NotFound'
+        ],
+        [
+            'path' => 'app/Domain/{{modelName}}',
+            'filename' => '{{modelName}}NotFound',
+            'stub' => 'NotFound'
+        ],
+        [
+            'path' => 'app/Domain/{{modelName}}',
+            'filename' => '{{modelName}}RepositoryInterface',
+            'stub' => 'RepositoryInterface'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Delete{{modelName}}',
+            'filename' => 'Delete{{modelName}}',
+            'stub' => 'Delete'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Delete{{modelName}}',
+            'filename' => 'Delete{{modelName}}Handler',
+            'stub' => 'DeleteHandler'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Get{{modelName}}List',
+            'filename' => 'Get{{modelName}}List',
+            'stub' => 'GetList'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Get{{modelName}}List',
+            'filename' => 'Get{{modelName}}ListHandler',
+            'stub' => 'GetListHandler'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Store{{modelName}}',
+            'filename' => 'Store{{modelName}}',
+            'stub' => 'Store'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Store{{modelName}}',
+            'filename' => 'Store{{modelName}}Handler',
+            'stub' => 'StoreHandler'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Update{{modelName}}',
+            'filename' => 'Update{{modelName}}',
+            'stub' => 'Update'
+        ],
+        [
+            'path' => 'app/Application/{{modelName}}/Update{{modelName}}',
+            'filename' => 'Update{{modelName}}Handler',
+            'stub' => 'UpdateHandler'
+        ],
+        [
+            'path' => 'app/Http/Requests/{{modelName}}',
+            'filename' => 'List{{modelName}}Request',
+            'stub' => 'ListRequest'
+        ],
+        [
+            'path' => 'app/Http/Requests/{{modelName}}',
+            'filename' => 'Store{{modelName}}Request',
+            'stub' => 'StoreRequest'
+        ],
+        [
+            'path' => 'app/Http/Requests/{{modelName}}',
+            'filename' => 'Update{{modelName}}Request',
+            'stub' => 'UpdateRequest'
+        ],
+        [
+            'path' => 'api-doc/collections/',
+            'filename' => '{{modelNamePluralLowerCaseHyphen}}',
+            'stub' => 'ApiDoc'
+        ],
+        [
+            'path' => 'database/migrations/',
+            'filename' => '{{migrationName}}',
+            'stub' => 'Migration'
+        ],
+        [
+            'path' => 'tests/Feature/',
+            'filename' => '{{modelName}}Test',
+            'stub' => 'Test'
+        ],
+        [
+            'path' => 'app/Http/Resources/{{modelName}}',
+            'filename' => '{{modelName}}Resource',
+            'stub' => 'Resource'
+        ],
+        [
+            'path' => 'app/Http/Resources/{{modelName}}',
+            'filename' => '{{modelName}}ResourceCollection',
+            'stub' => 'ResourceCollection'
+        ],
         [
             'path' => 'app/Infrastructure/Repositories',
             'filename' => '{{modelName}}Repository',
